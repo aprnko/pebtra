7c1414bac2ec8722ee510e0e00d9e3f683d8f931
Aleksei Protsenko <lexprie@gmail.com>
2026-01-01 17:00:45 +0500

    Updated LinkDirect stored procedure to handle uncategorized transactions

---
+++ pebtra.DAL/Migrations/20260101115247_UpdateLinkDirectProcedure.cs
@@ -0,0 +1,263 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pebtra.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLinkDirectProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE OR ALTER PROCEDURE LinkDirect
+    @Id1 INT,
+    @Id2 INT,
+    @Id3 INT = NULL,
+    @Id4 INT = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+	PRINT '>>> Executing LinkDirect'
+
+    DECLARE @NewGroupId UNIQUEIDENTIFIER = NEWID();
+
+    -- Build table of input IDs
+    DECLARE @Ids TABLE (Id INT);
+    INSERT INTO @Ids (Id) VALUES (@Id1), (@Id2);
+    IF @Id3 IS NOT NULL INSERT INTO @Ids VALUES (@Id3);
+    IF @Id4 IS NOT NULL INSERT INTO @Ids VALUES (@Id4);
+
+    -- Select all involved rows
+    DECLARE @Selected TABLE (
+        Id INT,
+        CategoryId CHAR(10),
+        Amount DECIMAL(18, 2),
+        GroupId UNIQUEIDENTIFIER,
+        TransactionTypeId INT,
+        Date DATE
+    );
+
+    INSERT INTO @Selected (Id, CategoryId, Amount, GroupId, TransactionTypeId, Date)
+    SELECT t.Id, t.CategoryId, t.Amount, t.GroupId, c.TransactionTypeId, t.Date
+    FROM Transactions t
+    INNER JOIN @Ids i ON t.Id = i.Id
+    LEFT OUTER JOIN Categories c ON t.CategoryId = c.Id;
+
+    DECLARE @CategoryId NVARCHAR(100) = (SELECT TOP 1 CategoryId FROM @Selected ORDER BY CategoryId DESC);    
+    PRINT 'Category is ' + @CategoryId
+    
+    IF (@CategoryId IS NULL)
+    BEGIN
+	    PRINT 'No category specified for any of the transactions. Assuming category is T_TRANSFER.'
+    	SET @CategoryID = 'T_TRANSFER' -- Default category for matched uncategorized transactions 
+    END
+   
+    -- Validation 1: all records must exist
+    IF (SELECT COUNT(*) FROM @Ids) != (SELECT COUNT(*) FROM @Selected)
+    BEGIN
+        RAISERROR('One or more specified transaction IDs do not exist.', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 2: all records must have GroupId = NULL
+    IF EXISTS (SELECT 1 FROM @Selected WHERE GroupId IS NOT NULL)
+    BEGIN
+        RAISERROR('One or more specified transactions already have a GroupId.', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 3: all records must have the same CategoryId
+    IF (SELECT COUNT(DISTINCT CategoryId) FROM @Selected WHERE CategoryId IS NOT NULL) > 1
+    BEGIN
+        RAISERROR('All specified transactions must have the same CategoryId.', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 4: TransactionTypeId must be 0 (Transfer) or 2 (Conversion)
+    DECLARE @TransactionTypeId INT = (SELECT TOP 1 TransactionTypeId FROM @Selected);
+    IF @TransactionTypeId NOT IN (-1, 0, 2)
+    BEGIN
+        RAISERROR('TransactionTypeId must be either Transfer (including Return), Conversion or Expense', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 5: Amounts sum must be less than 1 for TRANSFER transactions    
+    IF @CategoryId = 'T_TRANSFER' AND (SELECT ABS(SUM(Amount)) FROM @Selected) >= 1    
+    BEGIN
+        RAISERROR('The sum of amounts for the specified transactions must be less than 1 for TRANSFER transactions.', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 6: For Conversion transactions (TransactionTypeId = 2), there should be exactly 2 records, and amounts must have different signs
+    IF @TransactionTypeId = 2
+    BEGIN
+        DECLARE @Count INT = (SELECT COUNT(*) FROM @Selected);
+        IF @Count != 2
+        BEGIN
+            RAISERROR('For Conversion transactions, exactly 2 transactions are required.', 16, 1);
+            RETURN;
+        END
+
+        DECLARE @PositiveCount INT = (SELECT COUNT(*) FROM @Selected WHERE Amount > 0);
+        DECLARE @NegativeCount INT = (SELECT COUNT(*) FROM @Selected WHERE Amount < 0);
+
+        IF @PositiveCount != 1 OR @NegativeCount != 1
+        BEGIN
+            RAISERROR('For Conversion transactions, the amounts must have different signs (one positive and one negative).', 16, 1);
+            RETURN;
+        END
+    END
+    
+    -- Validation 7: Transactions should belong to the same month.
+    IF (SELECT COUNT(DISTINCT MONTH(Date)) FROM @Selected) > 1
+    BEGIN
+        RAISERROR('All specified transactions must belong to the same month. Use LinkTransit if the transactions being linked have been precessed in different months.', 16, 1);
+        RETURN;
+    END
+
+    PRINT 'All checks passed, updating GroupId.'
+    
+    -- All checks passed — update
+    UPDATE Transactions
+    SET GroupId = @NewGroupId
+    WHERE Id IN (SELECT Id FROM @Ids);
+   
+    -- For expense and return transactions, set CategoryId to T_RETURN
+    IF @TransactionTypeId = -1
+    BEGIN
+	    PRINT 'Setting category to T_RETURN for expense and return transactions.'
+	    UPDATE Transactions
+	    SET CategoryId = 'T_RETURN'
+	    WHERE Id IN (SELECT Id FROM @Ids);    	
+    END
+    
+    PRINT 'Setting category for transactions without a category (if any).'
+    -- For uncategorized transactions set CategoryId
+	UPDATE Transactions
+    SET CategoryId = @CategoryId
+    WHERE Id IN (SELECT Id FROM @Ids) AND CategoryId IS NULL;   
+   
+END;
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE OR ALTER PROCEDURE LinkDirect
+    @Id1 INT,
+    @Id2 INT,
+    @Id3 INT = NULL,
+    @Id4 INT = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @NewGroupId UNIQUEIDENTIFIER = NEWID();
+
+    -- Build table of input IDs
+    DECLARE @Ids TABLE (Id INT);
+    INSERT INTO @Ids (Id) VALUES (@Id1), (@Id2);
+    IF @Id3 IS NOT NULL INSERT INTO @Ids VALUES (@Id3);
+    IF @Id4 IS NOT NULL INSERT INTO @Ids VALUES (@Id4);
+
+    -- Select all involved rows
+    DECLARE @Selected TABLE (
+        Id INT,
+        CategoryId CHAR(10),
+        Amount DECIMAL(18, 2),
+        GroupId UNIQUEIDENTIFIER,
+        TransactionTypeId INT,
+        Date DATE
+    );
+
+    INSERT INTO @Selected (Id, CategoryId, Amount, GroupId, TransactionTypeId, Date)
+    SELECT t.Id, t.CategoryId, t.Amount, t.GroupId, c.TransactionTypeId, t.Date
+    FROM Transactions t
+    INNER JOIN @Ids i ON t.Id = i.Id
+    INNER JOIN Categories c ON t.CategoryId = c.Id;
+
+    -- Validation 1: all records must exist
+    IF (SELECT COUNT(*) FROM @Ids) != (SELECT COUNT(*) FROM @Selected)
+    BEGIN
+        RAISERROR('One or more specified transaction IDs do not exist.', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 2: all records must have GroupId = NULL
+    IF EXISTS (SELECT 1 FROM @Selected WHERE GroupId IS NOT NULL)
+    BEGIN
+        RAISERROR('One or more specified transactions already have a GroupId.', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 3: all records must have the same CategoryId
+    IF (SELECT COUNT(DISTINCT CategoryId) FROM @Selected) > 1
+    BEGIN
+        RAISERROR('All specified transactions must have the same CategoryId.', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 4: TransactionTypeId must be 0 (Transfer) or 2 (Conversion)
+    DECLARE @TransactionTypeId INT = (SELECT TOP 1 TransactionTypeId FROM @Selected);
+    IF @TransactionTypeId NOT IN (-1, 0, 2)
+    BEGIN
+        RAISERROR('TransactionTypeId must be either Transfer (including Return), Conversion or Expense', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 5: Amounts sum must be less than 1 for TRANSFER transactions    
+    DECLARE @CategoryId NVARCHAR  = (SELECT TOP 1 CategoryId FROM @Selected);
+    IF @CategoryId = 'T_TRANSFER' AND (SELECT ABS(SUM(Amount)) FROM @Selected) >= 1    
+    BEGIN
+        RAISERROR('The sum of amounts for the specified transactions must be less than 1 for TRANSFER transactions.', 16, 1);
+        RETURN;
+    END
+
+    -- Validation 6: For Conversion transactions (TransactionTypeId = 2), there should be exactly 2 records, and amounts must have different signs
+    IF @TransactionTypeId = 2
+    BEGIN
+        DECLARE @Count INT = (SELECT COUNT(*) FROM @Selected);
+        IF @Count != 2
+        BEGIN
+            RAISERROR('For Conversion transactions, exactly 2 transactions are required.', 16, 1);
+            RETURN;
+        END
+
+        DECLARE @PositiveCount INT = (SELECT COUNT(*) FROM @Selected WHERE Amount > 0);
+        DECLARE @NegativeCount INT = (SELECT COUNT(*) FROM @Selected WHERE Amount < 0);
+
+        IF @PositiveCount != 1 OR @NegativeCount != 1
+        BEGIN
+            RAISERROR('For Conversion transactions, the amounts must have different signs (one positive and one negative).', 16, 1);
+            RETURN;
+        END
+    END
+    
+    -- Validation 7: Transactions should belong to the same month.
+    IF (SELECT COUNT(DISTINCT MONTH(Date)) FROM @Selected) > 1
+    BEGIN
+        RAISERROR('All specified transactions must belong to the same month. Use LinkTransit if the transactions being linked have been precessed in different months.', 16, 1);
+        RETURN;
+    END
+
+    -- All checks passed — update
+    UPDATE Transactions
+    SET GroupId = @NewGroupId
+    WHERE Id IN (SELECT Id FROM @Ids);
+    
+    -- For expense and return transactions, set CategoryId to T_RETURN
+    IF @TransactionTypeId = -1
+    BEGIN
+	    UPDATE Transactions
+	    SET CategoryId = 'T_RETURN'
+	    WHERE Id IN (SELECT Id FROM @Ids);    	
+    END
+   
+END;");
+        }
+    }
+}
---
+++ pebtra.DAL/Migrations/20260101115717_AddCategoryIdToVValidateTransactions.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pebtra.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryIdToVValidateTransactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE OR ALTER VIEW V_ValidateTransactions AS 
+SELECT TOP 10000 * FROM (
+	SELECT 'Transfer amounts differ by more than 1.0' AS msg, t.GroupId, t.Id, t.Date, YEAR(t.Date) AS TYear, MONTH(t.Date) AS TMonth, t.AccountId, t.Amount, t.CategoryId, t.Details, t.Comment, t.ExtraDetails, t.CurrencyDetails 
+	FROM Transactions t
+	WHERE GroupId in (
+		select GroupId from (
+			select GroupId, Sum(Amount) as balance
+			from Transactions t
+			where t.CategoryId = 'T_TRANSFER' AND t.IsActive = 1
+			group by GroupId) q 
+		where balance > 1
+	)
+	UNION
+SELECT 'Returned amount differs from the original amount by more than 10%' AS msg, t.GroupId, t.Id, t.Date, YEAR(t.Date) AS TYear, MONTH(t.Date) AS TMonth, t.AccountId, t.Amount, t.CategoryId, t.Details, t.Comment, t.ExtraDetails, t.CurrencyDetails
+FROM Transactions t
+	WHERE GroupId in (
+		SELECT pos.GroupId 
+		FROM (
+			SELECT GroupId, Sum(Amount) as balance
+			FROM Transactions t
+			WHERE t.CategoryId = 'T_RETURN' AND t.Amount > 0 AND t.IsActive = 1
+			GROUP BY GroupId) AS pos
+		INNER JOIN (
+		SELECT GroupId, Sum(Amount) as balance
+			FROM Transactions t
+			WHERE t.CategoryId = 'T_RETURN' AND t.Amount < 0 AND t.IsActive = 1
+			GROUP BY GroupId
+		) AS neg 
+		ON pos.GroupId = neg.GroupId
+		WHERE ABS(1 - ABS(pos.balance / neg.balance)) >= 0.1
+	)
+	UNION	
+	SELECT 'Conversion rate differs from the market rate by more than 10%' AS msg, t.GroupId, t.Id, t.Date, YEAR(t.Date) AS TYear, MONTH(t.Date) AS TMonth, t.AccountId, t.Amount, t.CategoryId, t.Details, t.Comment, t.ExtraDetails, t.CurrencyDetails
+	FROM Transactions t
+	WHERE GroupId IN (
+		SELECT GroupId FROM V_GroupedConversions q
+		WHERE q.RateDifference > 1.1 OR q.RateDifference < 0.9
+	)
+	UNION
+	SELECT 'Ungrouped transfer or conversion' AS msg, t.GroupId, t.Id, t.Date, YEAR(t.Date) AS TYear, MONTH(t.Date) AS TMonth, t.AccountId, t.Amount, t.CategoryId, t.Details, t.Comment, t.ExtraDetails, t.CurrencyDetails
+	FROM Transactions t
+	WHERE GroupId IS NULL 
+	AND (t.CategoryId LIKE 'T_%' OR t.CategoryId = 'C_CONV')
+	UNION
+	SELECT 'Sum of transfer transactions for a month is not zero' AS msg, NULL, NULL, NULL, TYear, TMonth, NULL, AmountSum, NULL, CurrencyId, NULL, NULL, NULL
+	FROM (
+		SELECT YEAR(Date) AS TYear, MONTH (Date) AS TMonth, Sum(Amount) AS AmountSum, CurrencyId
+		FROM Transactions t
+		INNER JOIN Accounts a on a.Id = t.AccountId
+		WHERE t.CategoryId LIKE 'T_%'
+		GROUP BY YEAR(Date), MONTH (Date),  CurrencyId, t.CategoryId) q
+	WHERE ABS(AmountSum) > 1) q
+ORDER BY Date;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE OR ALTER           VIEW V_ValidateTransactions AS 
+SELECT TOP 10000 * FROM (
+	SELECT 'Transfer amounts differ by more than 1.0' AS msg, t.GroupId, t.Id, t.Date, YEAR(t.Date) AS TYear, MONTH(t.Date) AS TMonth, t.AccountId, t.Amount, t.Details, t.Comment, t.ExtraDetails, t.CurrencyDetails 
+	FROM Transactions t
+	WHERE GroupId in (
+		select GroupId from (
+			select GroupId, Sum(Amount) as balance
+			from Transactions t
+			where t.CategoryId = 'T_TRANSFER' AND t.IsActive = 1
+			group by GroupId) q 
+		where balance > 1
+	)
+	UNION
+SELECT 'Returned amount differs from the original amount by more than 10%' AS msg, t.GroupId, t.Id, t.Date, YEAR(t.Date) AS TYear, MONTH(t.Date) AS TMonth, t.AccountId, t.Amount, t.Details, t.Comment, t.ExtraDetails, t.CurrencyDetails
+FROM Transactions t
+	WHERE GroupId in (
+		SELECT pos.GroupId 
+		FROM (
+			SELECT GroupId, Sum(Amount) as balance
+			FROM Transactions t
+			WHERE t.CategoryId = 'T_RETURN' AND t.Amount > 0 AND t.IsActive = 1
+			GROUP BY GroupId) AS pos
+		INNER JOIN (
+		SELECT GroupId, Sum(Amount) as balance
+			FROM Transactions t
+			WHERE t.CategoryId = 'T_RETURN' AND t.Amount < 0 AND t.IsActive = 1
+			GROUP BY GroupId
+		) AS neg 
+		ON pos.GroupId = neg.GroupId
+		WHERE ABS(1 - ABS(pos.balance / neg.balance)) >= 0.1
+	)
+	UNION	
+	SELECT 'Conversion rate differs from the market rate by more than 10%' AS msg, t.GroupId, t.Id, t.Date, YEAR(t.Date) AS TYear, MONTH(t.Date) AS TMonth, t.AccountId, t.Amount, t.Details, t.Comment, t.ExtraDetails, t.CurrencyDetails
+	FROM Transactions t
+	WHERE GroupId IN (
+		SELECT GroupId FROM V_GroupedConversions q
+		WHERE q.RateDifference > 1.1 OR q.RateDifference < 0.9
+	)
+	UNION
+	SELECT 'Ungrouped transfer or conversion' AS msg, t.GroupId, t.Id, t.Date, YEAR(t.Date) AS TYear, MONTH(t.Date) AS TMonth, t.AccountId, t.Amount, t.Details, t.Comment, t.ExtraDetails, t.CurrencyDetails
+	FROM Transactions t
+	WHERE GroupId IS NULL 
+	AND (t.CategoryId LIKE 'T_%' OR t.CategoryId = 'C_CONV')
+	UNION
+	SELECT 'Sum of transfer transactions for a month is not zero' AS msg, NULL, NULL, NULL, TYear, TMonth, NULL, AmountSum, CurrencyId, NULL, NULL, NULL
+	FROM (
+		SELECT YEAR(Date) AS TYear, MONTH (Date) AS TMonth, Sum(Amount) AS AmountSum, CurrencyId
+		FROM Transactions t
+		INNER JOIN Accounts a on a.Id = t.AccountId
+		WHERE t.CategoryId LIKE 'T_%'
+		GROUP BY YEAR(Date), MONTH (Date),  CurrencyId, t.CategoryId) q
+	WHERE ABS(AmountSum) > 1) q
+ORDER BY Date;");
+        }
+    }
+}
